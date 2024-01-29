2a3840141f56ba44cc6ae90df84013bb3bd0b5b5
Diego La Faye <lafayediego@gmail.com.com>
2024-01-28 21:18:49 -0400

    added the numbers pipes page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -8,6 +8,14 @@ import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
 import { ProductsModule } from './products/products.module';
 
+
+//Config for the locale on the app
+import localEsBO from "@angular/common/locales/es-BO";
+import localOsRU from "@angular/common/locales/os-RU";
+import {registerLocaleData} from "@angular/common";
+
+registerLocaleData(localEsBO);
+registerLocaleData(localOsRU);
 @NgModule({
   declarations: [
     AppComponent
@@ -20,7 +28,9 @@ import { ProductsModule } from './products/products.module';
     BrowserAnimationsModule,
     ProductsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-BO'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/products/numbers-page/numbers-page.component.ts
+++ src/app/products/numbers-page/numbers-page.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
-
+  public totalSells:number = 265554.4431;
+  public percent: number = 0.4864
 }
